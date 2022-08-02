b689491b7ad2f822563d6e554509bd4420a1c4e5
cra123 <Onixmohamed@yahoo.fr>
2022-08-02 04:47:31 +0300

    Initial Position & Ball Movement

--- js/index.js
+++ js/index.js
@@ -7,14 +7,13 @@ window.onload = () => {
     restartGame();
   };
 
-  //Reload window and Clear Cache
+ 
+  const theCanvas = document.getElementById('canvas');
+  const ctx = theCanvas.getContext('2d');
 
-  function restartGame(){
-    window.location.reload();
-  }
-
-
-
+  canvas.width = theCanvas.width;
+  canvas.height = theCanvas.height;
+  
   //creating a new image
   const ball = new Image();
   ball.src = 'images/throw-ball.png';
@@ -28,9 +27,77 @@ window.onload = () => {
   const girlRight = new Image();
   girlRight.src = 'images/girl-right.png';
 
+  //initial speed 
+  let positionWidth = 40;
+  let reduceCounter = (theCanvas.width-60);
+
+  
+
 
   function startGame() {
+  setScene();
 
+  //Draw the ball on the screen
+  ctx.drawImage(ball, positionWidth, theCanvas.height/2, 30, 30);
+  
+  //Draw the fixed Girls
+  fixedGirlsPosition();
+  updateBallPosition();
+      
+  }
+  
+  //Clear the Canvas for next frame update
+  function clearCanvas(){
+    ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+  }
+
+  //update ball position
+  function updateBallPosition(){
+    if ((positionWidth) < (theCanvas.width-40)) {
+      positionWidth += 10;
+      clearCanvas();
+      fixedGirlsPosition();
+      ctx.drawImage(ball, positionWidth, theCanvas.height/2, 30, 30);
+      console.log(positionWidth);
+      if (positionWidth > (theCanvas.width-60)) {
+        reduceCounter = positionWidth;
+      }
+    }
+
+    // else if (reduceCounter < 0){
+    //   reduceCounter = positionWidth;
+    // }
+
+    else {
+      reduceCounter -= 10;
+      clearCanvas();
+      fixedGirlsPosition();
+      ctx.drawImage(ball, reduceCounter, theCanvas.height/2, 30, 30);
+      console.log(reduceCounter);
+      if (reduceCounter < 40){
+        positionWidth = reduceCounter
+      }
+    }
+
+    requestAnimationFrame(updateBallPosition);
+  }
+
+
+  //draw fixed girl on the screen
+  function fixedGirlsPosition(){
+    //Draw the girl-left on the screen
+    ctx.drawImage(girlRight, 10, theCanvas.height/2, 50, 50)
+    //Draw the girl-left on the screen
+    ctx.drawImage(girlLeft, (theCanvas.width - 60), theCanvas.height/2, 50, 50)
+  }
+
+  
+  //Reload window and Clear Cache
+  function restartGame(){
+    window.location.reload();
+  }
+
+  function setScene(){
   const startbutton = document.getElementById('start-button');
   const resetbutton = document.getElementById('restart-button');
   const endbutton = document.getElementById('end-button');
@@ -47,24 +114,5 @@ window.onload = () => {
   //Change div display to none
   const instructionDiv = document.getElementsByClassName('instruction');
   instructionDiv[0].style.display = 'none';
-
-
-  
-  
-
-  const theCanvas = document.getElementById('canvas');
-  const ctx = theCanvas.getContext('2d');
-
-  //Draw the ball on the screen
-  ctx.drawImage(ball, theCanvas.width/2, theCanvas.height/2, 50, 50);
-
-  //Draw the girl-left on the screen
-  ctx.drawImage(girlRight, theCanvas.width/4, theCanvas.height/2, 50, 50)
-
-  //Draw the girl-left on the screen
-  ctx.drawImage(girlLeft, theCanvas.width/2, theCanvas.height/4, 50, 50)
-
-
-      
-  }     
+  }
 };
